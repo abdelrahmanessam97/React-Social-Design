0ef6cc2ec6d21047fcfe3312d1fd2e28d84984a4
Abd Elrahman Essam Mohamed <112764287+abdelrahmanessam97@users.noreply.github.com>
2024-09-24 00:40:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { DarkModeContextProvider } from "./Context/darkModeContext.jsx";
 
+import "./Style.scss";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DarkModeContextProvider>
